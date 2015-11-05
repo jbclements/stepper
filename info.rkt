6a15f463f49782af1de21bd8a646b878fd27937b
John Clements <clements@racket-lang.org>
2015-11-05 06:39:01 -0800

    don't think this went anywhere, committing trailing changes before deleting local repo

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang setup/infotab
+(define version "1.0")
+
+(define drracket-tools '(("stepper+xml-tool.rkt")))
+
+(define drracket-tool-names (list "The Stepper"))
+
+(define drracket-tool-icons (list '("stepper-32x32.png" "icons")))
+
+#;(define scribblings '(("scribblings/stepper.scrbl")))
